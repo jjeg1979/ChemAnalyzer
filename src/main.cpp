6f24d4e3d06e4077b173437127640124558070cf
jjeg1979 <jjeg1979@gmail.com>
2015-11-19 22:08:29 +0100

    Added main program to start testing

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+/*! main.cpp
+ *         Program to test classes for ChemAnalyzer
+ *
+ *  Detailed description starts here.
+ */
+ 
+ #include <iostream>
+ #include "Terminal.h"
+ 
+ using namespace std;
+ using namespace Terminales;
+ 
+ int main(
+	 Lower_case Reactivo;
+	 
+	 std::cout << "Reactivo por defecto: " << Reactivo << '\n';
+	 return 0;
+ )
